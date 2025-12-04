65773d6581fae9e8218836d29dc3ad9f2fcdfa38
Fintra Dev <dev@fintradex.io>
2025-12-04 09:35:08 +0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::{fs::File, io::{BufReader, BufRead},collections::BTreeMap};
+
+fn main() {
+    let f=File::open("./data/measurements.txt").unwrap();
+let mut f=BufReader::new(f);
+let mut stats: BTreeMap<String,(f64, f64,usize, f64)> = BTreeMap::new();
+for line in f.lines(){
+    let line=line.unwrap();
+    let (station,temperature)=line.split_once(';').unwrap();
+    let temperature:f64=temperature.parse().unwrap();
+    let stats=stats.entry(station.to_string()).or_insert((f64::MAX,0.0,0,f64::MIN));
+    stats.0=stats.0.min(temperature);
+    stats.1+=temperature;
+    stats.2+=1;
+    stats.3=stats.3.max(temperature);
+}
+print!("{{");
+let mut stats=stats.into_iter().peekable();
+//for ((station,temperature), (min,max,count,sum)) in stats{
+while let Some((station,(min,sum,count,max)))=stats.next(){
+    print!("{station}={min}/{}{max}",sum/(count as f64));
+    if stats.peek().is_some(){
+        print!(",");
+    }
+}
+print!("}}");
+//}
+}
