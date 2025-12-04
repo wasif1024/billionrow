93a4c85cd472ba7d05e1e66020579013cd51525d
Fintra Dev <dev@fintradex.io>
2025-12-04 10:55:17 +0400

    Efficiency increased by removing Btreemap and .entry method

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
-use std::{fs::File, io::{BufReader, BufRead},collections::BTreeMap};
+use std::{fs::File, io::{BufReader, BufRead},collections::{HashMap, BTreeMap}};
 
 fn main() {
     let f=File::open("./data/measurements.txt").unwrap();
 let mut f=BufReader::new(f);
-let mut stats: BTreeMap<String,(f64, f64,usize, f64)> = BTreeMap::new();
+let mut stats: HashMap<String,(f64, f64,usize, f64)> = HashMap::new();
 for line in f.lines(){
     let line=line.unwrap();
     let (station,temperature)=line.split_once(';').unwrap();
     let temperature:f64=temperature.parse().unwrap();
-    let stats=stats.entry(station.to_string()).or_insert((f64::MAX,0.0,0,f64::MIN));
+    //let stats=stats.entry(station.to_string()).or_insert((f64::MAX,0.0,0,f64::MIN));
+    let stats=match stats.get_mut(station){
+        Some(stats)=>stats,
+        None=>{
+            stats.entry(station.to_string()).or_insert((f64::MAX,0.0,0,f64::MIN))
+        }
+    };
     stats.0=stats.0.min(temperature);
     stats.1+=temperature;
     stats.2+=1;
     stats.3=stats.3.max(temperature);
 }
 print!("{{");
+let mut stats=BTreeMap::from_iter(stats);
 let mut stats=stats.into_iter().peekable();
+
+//let mut stats=stats.into_iter().peekable();
 //for ((station,temperature), (min,max,count,sum)) in stats{
 while let Some((station,(min,sum,count,max)))=stats.next(){
     print!("{station}={min}/{}{max}",sum/(count as f64));
