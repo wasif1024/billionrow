272091aa26fb9d8baaf456e1817d58216f487c26
Fintra Dev <dev@fintradex.io>
2025-12-04 14:45:43 +0400

    mmap

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-use std::{fs::File, io::{BufReader, BufRead},collections::{HashMap, BTreeMap}};
+use std::{fs::File, io::{BufReader, BufRead},collections::{HashMap, BTreeMap}, os::unix::io::AsRawFd};
 
 fn main() {
     let f=File::open("./data/measurements.txt").unwrap();
-let mut f=BufReader::new(f);
+let map=mmap(&f);
+    //let mut f=BufReader::new(f);
 let mut stats: HashMap<Vec<u8>,(f64, f64,usize, f64)> = HashMap::new();
-for line in f.split(b'\n'){
-    let line=line.unwrap();
+for line in map.split(|c|*c==b'\n'){
+    let line=line;
+    if line.is_empty(){
+        break;
+    }
     let mut fields=line.rsplitn(2,|c|*c==b';');
-    let temperature=fields.next().unwrap();
-    let station=fields.next().unwrap();
+    //let temperature=fields.next().unwrap();
+    //let station=fields.next().unwrap();
+    let (Some(temperature),Some(station))=(fields.next(),fields.next()) else{
+        panic!("Invalid line: {:?}",unsafe {std::str::from_utf8_unchecked(line)});
+    };
     
     //let (station,temperature)=line.rsplit(b';').unwrap();
     // Safety promised
@@ -39,4 +46,27 @@ while let Some((station,(min,sum,count,max)))=stats.next(){
 }
 print!("}}");
 //}
+}
+fn mmap(f:&File)->&'_ [u8]{
+    let len=f.metadata().unwrap().len();
+    let mut mmap=unsafe {
+        let ptr=libc::mmap(
+            std::ptr::null_mut(),
+            len as libc::size_t,
+            libc::PROT_READ,
+            libc::MAP_SHARED,
+            f.as_raw_fd(),
+            0
+        );
+        if ptr == libc::MAP_FAILED {
+            panic!("{:?}",std::io::Error::last_os_error());
+        }else{
+            if libc::madvise(ptr, len as libc::size_t, libc::MADV_SEQUENTIAL) != 0 {
+                panic!("{:?}",std::io::Error::last_os_error());
+            }
+            return std::slice::from_raw_parts(ptr as *const u8, len as usize);
+        }
+        
+        //std::ptr::from_raw_parts(ptr, f.metadata().unwrap().len() as usize)
+    };
 }
